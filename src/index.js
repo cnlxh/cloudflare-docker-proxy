3b455ab2be1c0d495ec18dd0bbedd28b52e5dc55
Xiaohui Li <xiaohui_li@foxmail.com>
2024-06-13 15:04:00 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ addEventListener("fetch", (event) => {
 });
 
 const routes = {
-  "docker.libcuda.so": "https://registry-1.docker.io",
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.credclouds.com": "https://registry-1.docker.io",
+  "quay.credclouds.com": "https://quay.io",
+  "gcr.credclouds.com": "https://gcr.io",
+  "k8s-gcr.credclouds.com": "https://k8s.gcr.io",
+  "k8s.credclouds.com": "https://registry.k8s.io",
+  "ghcr.credclouds.com": "https://ghcr.io",
+  "cloudsmith.credclouds.com": "https://docker.cloudsmith.io",
 };
 
 function routeByHosts(host) {
